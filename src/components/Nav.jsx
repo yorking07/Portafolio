9ed5d598a294e110436faf0373a39dfb9b1c44f2
yorking07e~ <jorgek070604@gmail.com>
2024-02-06 19:55:25 -0500

    iconos

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,52 @@
-import React from 'react'
-import "../styles/nav.css"
+import React from "react";
+import "../styles/nav.css";
 
 const Nav = () => {
   return (
     <>
-        <nav className='nav-container'>
-            <span className='Logotipo-container'>
-                <img className='logotipo' src="https://c8.alamy.com/compes/2edwr2t/plantilla-de-logotipo-de-la-letra-j-monnogram-delicado-diseno-floral-logotipo-personal-diseno-vectorial-2edwr2t.jpg" alt="" />
-            </span>
-            <span className='Logos-de-contacto'>
-                <button className='github'>    <img src="" alt="" />     </button>
-                <button className='linkedin'>  <img src="" alt="" />         </button>
-                <button className='gmail'>     <img src="" alt="" />    </button>
-                <button className='whatsapp'>  <img src="" alt="" />         </button>
-            </span>
-        </nav>
+      <nav className="nav-container">
+        <img
+          className="logotipo"
+          src="https://c8.alamy.com/compes/2edwr2t/plantilla-de-logotipo-de-la-letra-j-monnogram-delicado-diseno-floral-logotipo-personal-diseno-vectorial-2edwr2t.jpg"
+          alt=""
+        />
+
+        <span className="Logos-de-contacto">
+          <a href="https://www.google.com" target="_blank" className="logo-de-contacto">
+            {" "}
+            <img
+              
+              src="https://cdn-icons-png.flaticon.com/128/5968/5968866.png"
+              alt=""
+            />{" "}
+          </a>
+          <a href="https://www.google.com" target="_blank" className="logo-de-contacto">
+            {" "}
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/3536/3536505.png"
+              alt=""
+            />{" "}
+          </a>
+          <a href="https://www.google.com" target="_blank" className="logo-de-contacto">
+            {" "}
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/5968/5968534.png"
+              alt=""
+            />{" "}
+          </a>
+          <a href="https://www.google.com" target="_blank" className="logo-de-contacto">
+            {" "}
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/2504/2504957.png"
+              alt=""
+            />{" "}
+          </a>
+        </span>
+      </nav>
     </>
+  );
+};
 
-  )
-}
+export default Nav;
 
-export default Nav
+
