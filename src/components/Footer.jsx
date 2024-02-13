9fc665ffcc03dd0ce536ea2613d9eb90935bd782
yorking07e~ <jorgek070604@gmail.com>
2024-02-13 13:07:23 -0500

    certificados

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import "../styles/footer.css"
 
 const Footer = () => {
   return (
-    <div>
-        <span>Certificados</span>
-        <ul>
-            <li><a href="">Fundamentos Basicos</a></li>
-            <li><a href="">FrontEnd con React</a></li>
-            <li><a href="">BackEnd con node</a></li>
-            <li><a href="">Certificado Full Stack</a></li>
+    <div className='footer'>
+        <span className='footer_span'>Certificados</span>
+        <ul className='footer_ul'>
+            <li className='footer_li'><a className='footer_ancore' href="https://certiwise.co/verify/95" target='blank'>Fundamentos Basicos</a></li>
+            <li className='footer_li'><a className='footer_ancore' href="https://certiwise.co/verify/346" target='blank'>FrontEnd con React</a></li>
+            <li className='footer_li'><a className='footer_ancore' href="" target='blank'>BackEnd con node</a></li>
+            <li className='footer_li'><a className='footer_ancore' href="" >Certificado Full Stack</a></li>
         </ul>
     </div>
   )
